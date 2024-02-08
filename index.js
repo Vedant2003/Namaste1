733dcbe0bf2210960624df997975a3b8c6fd3d8a
Vedant Agnihotri <agnihotrivedant25@gmail.com>
2024-02-08 18:47:59 +0530

    epsoide1

---
+++ index.js
@@ -0,0 +1,12 @@
+const heading=React.createElement("div",
+{id:"heading"},
+[React.createElement("h1",
+{id:"hj"},
+"kaise ho!"),
+React.createElement("h2",
+{id:124},
+"saab badiya")
+]
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
